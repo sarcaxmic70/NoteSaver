4e3472a9e1c00e2a15bdfc53516cc3e78cc4acd1
sarcaxmic70 <146446817+sarcaxmic70@users.noreply.github.com>
2023-09-30 12:21:42 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ let notes=document.querySelectorAll(".input_box");
 add_button.addEventListener("click",()=>{
     let input_box=document.createElement("p");
     // let date_time=document.createElement("p");
+    input_box.innerHTML="Click her to start typing....///...Notes will be saved automatically.";
+    input_box.classList.add("before_click");
     let img=document.createElement("img");
     // date_time.classList.add("date");
     input_box.classList.add("input_box");
@@ -26,9 +28,15 @@ notes_container.addEventListener("click",(e)=>{
         }
     else if(e.target.tagName==="P"){
         notes=document.querySelectorAll(".input_box");
+        e.target.classList.remove("before_click");
+        e.target.innerHTML=`<img src="./images/delete.png">`;
         notes.forEach(nt => {
             nt.onkeyup=function(){
                 // nt.querySelector(".date").innerHTML=daTe();
+                // if(nt.innerHTML!=`<img src="./images/delete.png">`){
+                //     nt.classList.add("before_click");
+                //     nt.innerHTML=`Click her to start typing....///...Notes will be saved automatically.<img src="./images/delete.png">`;
+                // }
                 updateStorage();
             }
         })
@@ -42,8 +50,9 @@ function onLoad(){
     notes=document.querySelectorAll(".input_box");
     notes.forEach(nt=>{
         // console.log(nt.innerHTML);
-        if(nt.innerHTML=='<img src="./images/delete.png">')
+        if(nt.innerHTML=='Click her to start typing....///...Notes will be saved automatically.<img src="./images/delete.png">' || nt.innerHTML==`<img src="./images/delete.png">`)
         nt.remove();
+        // console.log(nt.innerHTML);
     })
     updateStorage();
 }
